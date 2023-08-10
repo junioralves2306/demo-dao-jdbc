85386b6bcf9e34fb86dc017d67ae1a3af5f618d7
Junior Alves <junior.alves2306@gmail.com>
2023-08-10 09:05:28 -0300

    Classe Vendedor criada

--- src/App.java
+++ src/App.java
@@ -1,8 +1,15 @@
+import java.util.Date;
+
 import entidades.Departamento;
+import entidades.Vendedor;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Departamento obj = new Departamento(1, "Books");
-        System.out.println(obj);
+
+        Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 
+        3000.0, obj);
+
+        System.out.println(vendedor);
     }
 }
